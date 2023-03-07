31f1aa44ed0edbbfa7ff2e1aefca0e8ef83204f3
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-07 19:36:35 +0100

    Adding a new feature

--- integration-testing/routes/books.route.js
+++ integration-testing/routes/books.route.js
@@ -3,5 +3,7 @@ const express = require('express');
 const bookRoute = express.Router();
 
 bookRoute.get('/api/books', (req, res) => {
-    res.status(200)
+    res.status(200).json({
+        "message": "Welcome to the Book Router"
+    })
 });
