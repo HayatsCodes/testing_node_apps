5ca289613911ada5510a6db060de1678713f5fd8
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-07 19:39:26 +0100

    Updating the code

--- integration-testing/routes/books.route.js
+++ integration-testing/routes/books.route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const bookRoute = express.Rou;
+const bookRoute = express.Router();
 
 bookRoute.get('/api/books', (req, res) => {
     res.status(200).json({
         "message": "Welcome to the Book Router"
-    })
+    });
 });
