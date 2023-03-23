0e5478132333497d00277acdeb17e29fdd3b152d
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-23 15:29:06 +0100

    Refactoring the code

---
+++ 1-testing/routes/product.route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/product.controller");
