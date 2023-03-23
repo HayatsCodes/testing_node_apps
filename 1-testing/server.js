84a0533460d7512643401922c0bc9534cfb0c5c4
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-23 15:47:29 +0100

    Fixing a bug

--- 1-testing/server.js
+++ 1-testing/server.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 
 /* Connecting to the database and then starting the server. */
 mongoose
+
   .connect(process.env.MONGODB_URI)
   .then(() => {
     app.listen(PORT, console.log("Server started on port 5000"));
