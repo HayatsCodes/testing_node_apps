b5aba463d9e009b8bce8caa03730f63455da6ec7
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-23 15:57:45 +0100

    Fixing a bug

--- 1-testing/server.js
+++ 1-testing/server.js
@@ -2,12 +2,16 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const PORT = process.env.PORT
 const MONGO_URL = process.env.MONGO_URL
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
 
 require("dotenv").config();
 
 /* Connecting to the database and then starting the server. */
 mongoose
-  .connect(MONGO_URL,)
+  .connect(MONGO_URL, options)
   .then(() => {
     app.listen(PORT, console.log("Server started on port 5000"));
   })
