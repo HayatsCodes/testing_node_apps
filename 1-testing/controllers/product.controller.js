9c47374a18b002651998f2888efe243e4acea229
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-23 15:19:06 +0100

    Adding a new feature

--- 1-testing/controllers/product.controller.js
+++ 1-testing/controllers/product.controller.js
@@ -20,6 +20,9 @@ const getProduct = async (req, res) => {
 
 const createProduct = async (req, res) => {
     try {
-        const product = await Product
-    }
+        const product = await Product.create(req.body);
+        res.status(201).json(product);
+    } catch (error) {
+        res.status(500).json(error);
+      }
 }
