03e5e26166900e5b57e438c1ee5b54aa842bad51
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-23 14:51:05 +0100

    Updating the documentation

--- app.test.js
+++ app.test.js
@@ -18,7 +18,7 @@ describe('Test example', () => {
                 return done();
             })
     }, 10000);
-    
+
     test('POST /send', (done) => {
         request(app)
             .post('/send')
@@ -31,44 +31,46 @@ describe('Test example', () => {
                 res.body.data.length = 2;
                 res.body.data[0].email = "test@example.com";
                 res.body.data[1].email = "francisco@example.com";
-              })
-              .end((err, res) => {
+            })
+            .end((err, res) => {
                 if (err) return done(err);
                 elementId = res.body.data[1].id;
                 return done();
-              });
+            });
     }, 10000);
 
     test('PUT /update/id', (done) => {
         request(app)
-        .put(`/update/${elementId}`)
-        .expect('Content-Type', /json/)
-        .send({
-            email: 'hayatscodes@gmail.com'
-        })
-        .expect(200)
-        .expect((res) => {
+            .put(`/update/${elementId}`)
+            .expect('Content-Type', /json/)
+            .send({
+                email: 'hayatscodes@gmail.com'
+            })
+            .expect(200)
+            .expect((res) => {
                 res.body.data.length = 2;
                 res.body.data[0].email = "test@example.com";
                 res.body.data[1].id = elementId;
                 res.body.data[1].email = "hayatscodes@gmail.com";
-        })
-        .end((err, res) => {
-            if (err) return done(err);
-            return done();
-        });
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            });
     }, 10000)
 
     test('DELETE destroy/id', (done) => {
         request(app)
-        .delete(`/destroy/${elementId}`)
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .expect((res) => {
-            res.body.data.length = 1;
-            res.body.data[0].email = "test@example.com";
-            res.body.data[1].id = elementId;
-            res.body.data[1].email = "hayatscodes@gmail.com";
-    })
-    })
+            .delete(`/destroy/${elementId}`)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect((res) => {
+                res.body.data.length = 1;
+                res.body.data[0].email = "test@example.com";
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            });
+    }, 10000)
 });
