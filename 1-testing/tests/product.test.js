556394f1cc0a4d5b03f9f60db9df51b8b2ddbb49
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-24 23:41:55 +0100

    Refactoring the code

---
+++ 1-testing/tests/product.test.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const request = require("supertest");
+const app = require('../app');
+
+require('dotenv').config();
+
+beforeEach(async () => {
+    await mongoose.connect(process.env.MONGODB_URL)
+});
+
+afterEach(async () => {
+    await mongoose.connection.close();
+});
