86dcbed3a9bdbe41a3ff3ebe2fd1fcc4c4b6c2ae
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-24 23:49:04 +0100

    Updating the documentation

--- 1-testing/tests/product.test.js
+++ 1-testing/tests/product.test.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const request = require("supertest");
 require('dotenv').config();
-
+console.log(process.env.MONGODB_URL);
 const app = require('../app');
 
 
